0ff0ea61cdf43e0b1baa70eb61aebf6fd96e0323
elisa stater <elisa_60@msn.com>
2018-10-17 17:12:30 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,19 @@
+
+var playlist = {
+ 
+     artistName1:"Slowdive",
+	   songTitle1:"Allison",
+     artistName2:'My Bloody Valentine',
+     songTitle2:'Sometimes,' 
+
+};
+
+ 
+function updatePlaylist(playlist, artistName3, songTitle3) {
+playlist[artistName3] = "Phil Buchs"; 
+playlist[songTitle3] ="Here's to the State of Mississipi";
+
+   return this.playlist;
+}
+console.log(playlist)
+
